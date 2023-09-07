fea7aa76baa6e1127328b58733836c021779469a
Ibrahim Nasr <42195782+inasribrahim@users.noreply.github.com>
2023-09-07 08:39:35 -0700

    delete absolute path comment

--- src/test/java/com/foodics/api/properties/PropertiesHandler.java
+++ src/test/java/com/foodics/api/properties/PropertiesHandler.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.util.Properties;
 
 public final class PropertiesHandler {
-// C:\Foodics\api_task\api-foodics-task\resources\config\api_config.properties
     private static final String ROOT_PATH = System.getProperty("user.dir")
             .concat("/resources/config/");
     private static final String API_CONFIG_PATH=ROOT_PATH.concat("api_config.properties");
